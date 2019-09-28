7ffaf9a64900b1793082d61002062f9fcd4d07a0
Jason <jason.lm@live.com>
2019-09-28 09:10:47 -0400

    Initial Commit

---
+++ Animate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animate
+//
+//  Created by Jayce Lee on 9/28/19.
+//  Copyright © 2019 Jason Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
